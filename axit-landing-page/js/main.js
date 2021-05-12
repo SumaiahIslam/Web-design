b8b6007c3bbfbc05f920c45ad62dafb0f4a9f366
SumaiahIslam <67160749+SumaiahIslam@users.noreply.github.com>
2021-05-12 20:46:20 +0600

    Add files via upload

---
+++ axit-landing-page/js/main.js
@@ -0,0 +1,46 @@
+//Tab functionality
+$(document).ready(function(){
+	
+    $('.tab-one').addClass('active');
+
+	$('.one-content').show();
+	$('.two-content').hide();
+	$('.three-content').hide();
+
+	$('.tab-one').click(function(){
+		$('.one-content').show();
+		$('.two-content').hide();
+		$('.three-content').hide();
+
+		//Add class Active
+		$('.tab-one').addClass('active');
+		$('.tab-two').removeClass('active');
+		$('.tab-three').removeClass('active');
+	});
+
+	
+	$('.tab-two').click(function(){
+		$('.one-content').hide();
+		$('.two-content').show();
+		$('.three-content').hide();
+
+		//Add class Active
+		$('.tab-one').removeClass('active');
+		$('.tab-two').addClass('active');
+		$('.tab-three').removeClass('active');
+	});
+
+
+    $('.tab-three').click(function(){
+		$('.one-content').hide();
+		$('.two-content').hide();
+		$('.three-content').show();
+
+		//Add class Active
+		$('.tab-one').removeClass('active');
+		$('.tab-two').removeClass('active');
+		$('.tab-three').addClass('active');
+	});
+	
+
+});
